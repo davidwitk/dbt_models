c14bb67634755dfa271b8014fcf40e4710bd1968
David Witkowski <43667775+chodera@users.noreply.github.com>
2023-01-11 22:18:42 +0100

    Fix bug

--- models/staging/stg_imdb_movies2.sql
+++ models/staging/stg_imdb_movies2.sql
@@ -28,7 +28,7 @@ final as (
 
     select
         *,
-        row_number() over (partition by title order by extracted_at desc) = 1 as is_latest_day
+        row_number() over (partition by title order by extracted_at desc) = 1 as is_latest_day,
         min(extracted_at :: date) over () as first_extraction_day_overall,
         min(extracted_at :: date) over (partition by title) as first_extraction_day
     from base
