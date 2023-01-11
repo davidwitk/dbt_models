d5904e8c1ee1cf6848310cd56162f7dd66a30c82
David Witkowski <43667775+chodera@users.noreply.github.com>
2023-01-11 22:10:49 +0100

    Update schema config to conform with CI

--- macros/get_custom_schema.sql
+++ macros/get_custom_schema.sql
@@ -1,3 +1,14 @@
 {% macro generate_schema_name(custom_schema_name, node) %}
-    {{ generate_schema_name_for_env(custom_schema_name, node) }}
+
+    {%- if target.name == ci -%}
+        {%- set default_schema = target.schema -%}
+        {%- if custom_schema_name is none -%}
+            {{ default_schema }}
+        {%- else -%}
+            {{ default_schema }}_{{ custom_schema_name | trim }}
+        {%- endif -%}
+    {%- else -%}
+        {{ generate_schema_name_for_env(custom_schema_name, node) }}
+    {%- endif -%}
+
 {% endmacro %}
