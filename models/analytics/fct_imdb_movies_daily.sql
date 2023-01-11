c0b077a6f354569719a743f417c797a5ef824b62
David Witkowski <43667775+chodera@users.noreply.github.com>
2023-01-11 21:36:45 +0100

    Fix linting issues

--- models/analytics/fct_imdb_movies_daily.sql
+++ models/analytics/fct_imdb_movies_daily.sql
@@ -6,20 +6,20 @@ movies as (
 
 final as (
 
-    select 
+    select
         id,
         extracted_at,
-        
-        title, 
+
+        title,
         rank,
-        rating, 
+        rating,
         rating_count,
         year,
         link,
 
         is_latest_day,
         first_extraction_day_overall,
-        first_extraction_day 
+        first_extraction_day
     from movies
 
 )
