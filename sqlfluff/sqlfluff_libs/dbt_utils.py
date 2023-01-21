83838c8cde2f2e3ccc452b62f058406742f0ebb8
David Witkowski <43667775+chodera@users.noreply.github.com>
2023-01-21 13:02:34 +0100

    Add dbt utils package

---
+++ sqlfluff/sqlfluff_libs/dbt_utils.py
@@ -0,0 +1,17 @@
+def date_spine(*args, **kwargs):
+    return 'select * from relation'
+
+def generate_series(*args, **kwargs):
+    return '1'
+
+def generate_surrogate_key(*args, **kwargs):
+    return 'key'
+
+def unpivot(*args, **kwargs):
+    return 'select * from relation'
+
+def get_query_results_as_dict(*args, **kwargs):
+    return {}
+
+def union_relations(*args, **kwargs):
+    return 'select * from relation'
